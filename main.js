e4f81ead10599d0153ebe353e3a73287e2dbed78
Ramzan Chasygov <theliketurbo@gmail.com>
2018-03-16 16:02:47 +0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-/* Some code */
-ctx.drawImage(/* Some drawing */)
-const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-const disData = new FilterDistortion().filter(data);
-const disImage = new window.ImageData(disData, canvas.width, canvas.height);
-ctx.putImageData(disImage, 0, 0);
-/* Some other code */
